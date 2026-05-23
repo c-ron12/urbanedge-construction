48be6f6081ecc816fd2052ad7fb321aeb2effd45
c-ron12 <pioneerboy912@gmail.com>
2026-05-23 09:39:23 +0545

    feat: import bootstrap icons globally

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { AuthProvider } from './components/backend/context/Auth.jsx';   // Import AuthProvider to wrap the App component.
 
 createRoot(document.getElementById('root')).render(
